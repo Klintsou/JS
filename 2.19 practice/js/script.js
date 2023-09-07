afc1f5a0eca9cbcc73a1724a554f4ba886bb5f80
Yury Klintsou <yuriklintsou@gmail.com>
2023-09-08 00:24:49 +0300

    2.24 cycles practice task 2

--- 2.19 practice/js/script.js
+++ 2.19 practice/js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const lines = 5;
 let result = '';
 // Проверяется именно переменная result, формируйте строку в ней
@@ -7,9 +9,9 @@ for (let i = 0; i <= lines + 0; i++) {
    starsCounter = i + i + 1
    for (let j = 0; j <= lines + i; j++) {
       if (j >= (lines + i + 1 - starsCounter)) {
-         result = result + '*'
+         result = result + '*';
       } else {
-         result = result + ' '
+         result = result + ' ';
       }
    }
    result = result + '\n'
