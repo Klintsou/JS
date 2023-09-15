6b0970906472235d83f2f418ae599ff17f14ee4b
Yury Klintsou <yuriklintsou@gmail.com>
2023-09-16 00:05:19 +0300

    4.72 Food project modal window

--- 3. Java in work/Project Food/src/js/main.js
+++ 3. Java in work/Project Food/src/js/main.js
@@ -106,4 +106,50 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('.timer', deadline);
+
+    // Modal
+    //[] - дата аттрибут
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', function() {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // Либо вариант с toggle - но тогда назначить класс в верстке
+            //modal.classList.toggle('show');
+            //убирает скролл
+            document.body.style.overflow = 'hidden';
+            //17 px ширина скролла
+            document.body.style.marginRight = '17px';
+            
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // Либо вариант с toggle - но тогда назначить класс в верстке
+        //возвращает скролл
+        document.body.style.overflow = '';
+        document.body.style.marginRight = '0px';
+    }
+    
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    //закрывать если нажимать на окно вне модальной формы
+    //потому что modal - это окно
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    //когда кликаем на Escape
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) { 
+            closeModal();
+        }
+    });
 });
