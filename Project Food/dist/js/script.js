695a95845fa1527accef6466235aec880c22bed0
Yury Klintsou <yuriklintsou@gmail.com>
2023-09-20 17:04:25 +0300

    5.98 Food project calories calculator with local storage and regex

--- Project Food/dist/js/script.js
+++ Project Food/dist/js/script.js
@@ -544,35 +544,57 @@ window.addEventListener('DOMContentLoaded', function () {
   // Calculator
 
   const result = document.querySelector('.calculating__result span');
-  let sex = 'female',
-    height,
-    weight,
-    age,
+  let sex, height, weight, age, ratio;
+  if (localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+  } else {
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+  }
+  if (localStorage.getItem('ratio')) {
+    ratio = localStorage.getItem('ratio');
+  } else {
     ratio = 1.375;
+    localStorage.setItem('ratio', 1.375);
+  }
   function calcTotal() {
     //проверка что все данные внесены
     if (!sex || !height || !weight || !age || !ratio) {
       result.textContent = '____';
       return;
     }
-    //формула из этой статьи https://fitseven.ru/zdorovie/metabolism/sutochnaya-norma-kaloriy
     if (sex === 'female') {
+      //формула из этой статьи https://fitseven.ru/zdorovie/metabolism/sutochnaya-norma-kaloriy
       result.textContent = Math.round((447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio);
     } else {
       result.textContent = Math.round((88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio);
     }
   }
   calcTotal();
-
-  //получение статической информации с селектора
-  function getStaticInformation(parentSelector, activeClass) {
-    const elements = document.querySelectorAll(`${parentSelector} div`);
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(elem => {
+      elem.classList.remove(activeClass);
+      if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+        elem.classList.add(activeClass);
+      }
+      if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+        elem.classList.add(activeClass);
+      }
+    });
+  }
+  initLocalSettings('#gender div', 'calculating__choose-item_active');
+  initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+  function getStaticInformation(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
     elements.forEach(elem => {
       elem.addEventListener('click', e => {
         if (e.target.getAttribute('data-ratio')) {
           ratio = +e.target.getAttribute('data-ratio');
+          localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
         } else {
           sex = e.target.getAttribute('id');
+          localStorage.setItem('sex', e.target.getAttribute('id'));
         }
         elements.forEach(elem => {
           elem.classList.remove(activeClass);
@@ -582,13 +604,20 @@ window.addEventListener('DOMContentLoaded', function () {
       });
     });
   }
-  getStaticInformation('#gender', 'calculating__choose-item_active');
-  getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+  getStaticInformation('#gender div', 'calculating__choose-item_active');
+  getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
-  //информацмация с input
+  //data from inputs
   function getDynamicInformation(selector) {
     const input = document.querySelector(selector);
     input.addEventListener('input', () => {
+      //regex only numbers
+      if (input.value.match(/\D/g)) {
+        //обводит красным
+        input.style.border = "1px solid red";
+      } else {
+        input.style.border = 'none';
+      }
       switch (input.getAttribute('id')) {
         case "height":
           height = +input.value;
--- Project Food/src/js/main.js
+++ Project Food/src/js/main.js
@@ -582,9 +582,22 @@ window.addEventListener('DOMContentLoaded', function() {
     // Calculator
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female',
-        height, weight, age,
+    
+    let sex, height, weight, age, ratio;
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
         ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
 
     function calcTotal() {
         //проверка что все данные внесены
@@ -592,8 +605,8 @@ window.addEventListener('DOMContentLoaded', function() {
             result.textContent = '____';
             return;
         }
-        //формула из этой статьи https://fitseven.ru/zdorovie/metabolism/sutochnaya-norma-kaloriy
         if (sex === 'female') {
+            //формула из этой статьи https://fitseven.ru/zdorovie/metabolism/sutochnaya-norma-kaloriy
             result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
         } else {
             result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
@@ -602,16 +615,34 @@ window.addEventListener('DOMContentLoaded', function() {
 
     calcTotal();
 
-    //получение статической информации с селектора
-    function getStaticInformation(parentSelector, activeClass) {
-        const elements = document.querySelectorAll(`${parentSelector} div`);
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            }
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
 
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 if (e.target.getAttribute('data-ratio')) {
                     ratio = +e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', e.target.getAttribute('id'));
                 }
     
                 elements.forEach(elem => {
@@ -625,16 +656,23 @@ window.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
-    //информацмация с input
+    //data from inputs
     function getDynamicInformation(selector) {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+            //regex only numbers
+            if (input.value.match(/\D/g)) {
+                //обводит красным
+                input.style.border = "1px solid red";
+            } else {
+                input.style.border = 'none';
+            }
             switch(input.getAttribute('id')) {
-                case "height":  
+                case "height":
                     height = +input.value;
                     break;
                 case "weight":
@@ -652,5 +690,4 @@ window.addEventListener('DOMContentLoaded', function() {
     getDynamicInformation('#height');
     getDynamicInformation('#weight');
     getDynamicInformation('#age');
-
 });
